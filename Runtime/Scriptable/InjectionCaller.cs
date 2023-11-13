c782fe03dd61bce4c0714780f54e0e023dc432a3
AdrianBotana <adruian@gmail.com>
2023-11-13 17:43:16 +0100

    Update InjectionCaller.cs

--- Runtime/Scriptable/InjectionCaller.cs
+++ Runtime/Scriptable/InjectionCaller.cs
@@ -1,11 +1,13 @@
 using TNRD;
+using System.Linq;
 using UnityEngine;
 using System.Collections.Generic;
 
 namespace Adruian.CodeInjection
 {
-    public class InjectionCaller<T> : MonoBehaviour
+    public class InjectionCaller<T> : MonoBehaviour, IDataInjector
     {
+        public string Type => string.Empty;
         [SerializeField] private InjectionScope<T> scriptable;
         [SerializeField] private List<SerializableInterface<IDataCaller<T>>> callers = new List<SerializableInterface<IDataCaller<T>>>();
 
@@ -24,6 +26,24 @@ namespace Adruian.CodeInjection
             {
                 if (listener.TryGetValue(out IDataCaller<T> listenerValue))
                     scriptable.RemoveCaller(listenerValue);
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (callers.Count < 1)
+                FindAllListeners();
+        }
+
+        public void FindAllListeners()
+        {
+            callers.Clear();
+            var dataCallers = FindObjectsOfType<MonoBehaviour>().OfType<IDataCaller<T>>();
+            foreach (IDataCaller<T> dataCaller in dataCallers)
+            {
+                SerializableInterface<IDataCaller<T>> serializableDataCaller = new SerializableInterface<IDataCaller<T>>();
+                serializableDataCaller.Value = dataCaller;
+                callers.Add(serializableDataCaller);
             }
         }
     }
